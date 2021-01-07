cdf58e1958166b3ecb0ce9f27a85113846d263d2
Tom Lee <win981026@gmail.com>
2021-01-07 23:16:50 +0900

    Added apollo on frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,19 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
