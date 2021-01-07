416468aa87f037ed2558e866441788802533445e
Tom Lee <win981026@gmail.com>
2021-01-07 23:24:35 +0900

    Added getBooksQuery on client

--- web/src/components/BookList.js
+++ web/src/components/BookList.js
@@ -1,9 +1,35 @@
+import { useQuery, gql } from '@apollo/client';
+
+const getBooksQuery = gql`
+  {
+    books {
+      id
+      name
+    }
+  }
+`
+
+const displayBooks = (books) => {
+  return (
+    <ul id="book-list">
+      {books.map(book => (
+        <li key={book.id}>{book.name}</li>
+      ))}
+    </ul>
+  )
+}
+
 function BookList() {
+  const { loading, error, data } = useQuery(getBooksQuery)
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const { books } = data;
+
   return (
     <div>
-        <ul id="book-list">
-          <li>book name</li>
-        </ul>
+      {displayBooks(books)}
     </div>
   );
 }
